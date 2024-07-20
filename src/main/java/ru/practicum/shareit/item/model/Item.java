894f2ff04e35a69b0e092742a90694b5826b0a50
norandex <norilsk.andrei@yandex.ru>
2024-07-20 13:15:59 +0300

    14th sprint, 1st iter

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.NotNull;
 @Table(name = "items")
 public class Item {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty
