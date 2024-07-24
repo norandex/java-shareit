4ea38bdbf620698ac129882bcf2dbbe569697ad2
norandex <norilsk.andrei@yandex.ru>
2024-07-24 08:13:52 +0300

    15th sprint, 1st iter

--- src/test/java/ru/practicum/shareit/booking/model/StateModelTest.java
+++ src/test/java/ru/practicum/shareit/booking/model/StateModelTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StateModelTest {
     @Test
-    void StateFromTest() {
+    void stateFromTest() {
         String stateString = "FUTURE";
 
         assertEquals(State.from(stateString).orElseThrow(() -> new InvalidStatusException("invalid Status")), State.FUTURE);
